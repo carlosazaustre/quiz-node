f94013f44bb7831a0cd1f0019b587a2374fa2967
Carlos Azaustre <cazaustre@gmail.com>
2015-06-07 22:31:38 +0200

    add author page

--- routes/index.js
+++ routes/index.js
@@ -11,4 +11,8 @@ router.get('/', function(req, res) {
 router.get('/quizes/question', quizController.question);
 router.get('/quizes/answer', quizController.answer);
 
+router.get('/author', function(req, res) {
+  res.render('author');
+});
+
 module.exports = router;
