84f8a0acf163c64d6cb47bc435de4ad5db12d776
Carlos Azaustre <cazaustre@gmail.com>
2015-06-08 11:55:53 +0200

    adapt models

--- models/models.js
+++ models/models.js
@@ -1,12 +1,28 @@
 var path = require('path');
 
+// Postgres DATABASE_URL = postgres://user:password@host:port/database
+// SQLite   DATABASE_URL = sqlite://:@:/
+var url = process.env.DATABASE_URL.match(/(.*)\:\/\/(.*?)\:(.*)@(.*)\:(.*)\/(.*)/);
+var DB_name   = ( url[6] || null );
+var user      = ( url[2] || null );
+var pwd       = ( url[3] || null );
+var protocol  = ( url[1] || null );
+var dialect   = ( url[1] || null );
+var port      = ( url[5] || null );
+var host      = ( url[4] || null );
+var storage   = process.env.DATABASE_STORAGE;
+
 // Modelo ORM
 var Sequelize = require('sequelize');
 
-// Usar BBDD SQLite
-var sequelize = new Sequelize(null, null, null, {
-  dialect: "sqlite",
-  storage: "quiz.sqlite"
+// Usar BBDD SQLite o PostgreSQL
+var sequelize = new Sequelize(DB_name, user, pwd, {
+  dialect   : protocol,
+  protocol  : protocol,
+  port      : port,
+  host      : host,
+  storage   : storage, // solo SQLite (.env)
+  omitNull  : true    // solo PostgreSQL
 });
 
 // Importar la definición de la tabla Quiz
