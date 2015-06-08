b45c67a58904465d0f7501179430c49b69e5ea76
Carlos Azaustre <cazaustre@gmail.com>
2015-06-08 13:04:33 +0200

    add autoload

--- routes/index.js
+++ routes/index.js
@@ -3,16 +3,20 @@ var router = express.Router();
 
 var quizController = require('../controllers/quiz_controller');
 
-/* GET home page. */
+// GET home page
 router.get('/', function(req, res) {
   res.render('index', { title: 'Quiz' });
 });
+
+// Autoload de comando con :quizId
+router.param('quizId', quizController.load);
 
 // Definición de rutas de /quizes
 router.get('/quizes', quizController.index)
 router.get('/quizes/:quizId(\\d+)', quizController.show);
 router.get('/quizes/:quizId(\\d+)/answer', quizController.answer);
 
+// GET authors
 router.get('/author', function(req, res) {
   res.render('author');
 });
