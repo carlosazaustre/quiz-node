dbedae7ba30f3426e31205386d1c4f379e829bc2
Carlos Azaustre <cazaustre@gmail.com>
2015-06-08 15:51:13 +0200

    create new view to create new questions

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -56,3 +56,27 @@ exports.answer = function (req, res) {
   });
 
 }
+
+// GET /quizes/new
+exports.new = function (req, res) {
+  var quiz = models.Quiz.build({
+    pregunta  : "Pregunta",
+    respuesta : "Respuesta"
+  });
+
+  res.render('quizes/new', { quiz: quiz });
+}
+
+// POST /quizes/create
+exports.create = function (req, res) {
+  var quiz = models.Quiz.build( req.body.quiz );
+
+  // Guarda en DB los campos "pregunta" y "respuesta" de quiz
+  quiz
+    .save({
+      fields: ["pregunta", "respuesta"]
+    })
+    .then(function () {
+      res.redirect('/quizes');
+    })
+}
--- routes/index.js
+++ routes/index.js
@@ -12,9 +12,11 @@ router.get('/', function(req, res) {
 router.param('quizId', quizController.load);
 
 // Definición de rutas de /quizes
-router.get('/quizes', quizController.index)
-router.get('/quizes/:quizId(\\d+)', quizController.show);
-router.get('/quizes/:quizId(\\d+)/answer', quizController.answer);
+router.get('/quizes',                       quizController.index)
+router.get('/quizes/:quizId(\\d+)',         quizController.show);
+router.get('/quizes/:quizId(\\d+)/answer',  quizController.answer);
+router.get('/quizes/new',                   quizController.new);
+router.get('/quizes/create',                quizController.create);
 
 // GET authors
 router.get('/author', function(req, res) {
