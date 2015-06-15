d4f24b457b8c14f6585b6eeaa9fb62138e18a7bd
Carlos Azaustre <cazaustre@gmail.com>
2015-06-15 16:50:27 +0200

    add login required middleware

--- controllers/session_controller.js
+++ controllers/session_controller.js
@@ -1,5 +1,14 @@
 var userController = require('./user_controller');
 
+// Middleware de autorización de accesos HTTP restingidos
+exports.loginRequired = function (req, res, next) {
+  if (req.session.user) {
+    next();
+  } else {
+    res.redirect('/login');
+  }
+}
+
 // GET /login -- Formulario de login
 exports.new = function (req, res) {
   var errors = req.session.errors || {};
